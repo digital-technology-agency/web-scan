26417be82119ee7225801286ee3d330a295d8a44
deemak <deema_k@mail.ru>
2021-08-30 21:01:36 +0300

    close #11 close #2 Сборка релиза.

--- cmd/distr/wscan.go
+++ cmd/distr/wscan.go
@@ -8,71 +8,85 @@ import (
 	"github.com/digital-technology-agency/web-scan/pkg/services/generators"
 	"github.com/digital-technology-agency/web-scan/pkg/services/json"
 	"github.com/digital-technology-agency/web-scan/pkg/utils"
+	"github.com/zenthangplus/goccm"
 	"net/http"
-	"os"
-	"sync"
+	"runtime"
 )
 
+var (
+	CORЕ_COUNT        = flag.String(`core_count`, "1", `Example 1`)
+	ALPHABET          = flag.String(`alphabet`, "", `Example abcdefg`)
+	URL_LEN           = flag.String(`len`, "", `Example 2`)
+	CUNCURRENCY_COUNT = flag.String(`concurrency`, "10", `Example 10`)
+	PROTOCOLS         = []string{"http", "https"}
+)
+
+func genWritersProtocols(names []string) map[string]*json.EachRowWriter {
+	result := map[string]*json.EachRowWriter{}
+	for _, name := range names {
+		writer, _ := json.NewEachRowWriter(fmt.Sprintf("%s.txt", name))
+		result[name] = writer
+	}
+	return result
+}
+
 func main() {
-	alphabet := flag.String(`alphabet`, "", `Example abcdefg`)
-	urlLen := flag.String(`len`, "", `Example 2`)
 	flag.Parse()
 	/*check flags*/
-	if *alphabet == "" && *urlLen == "" {
+	if *ALPHABET == "" && *URL_LEN == "" {
 		flag.PrintDefaults()
 		return
 	}
+	runtime.GOMAXPROCS(utils.Int(*CORЕ_COUNT))
+	cuncurency := goccm.New(utils.Int(*CUNCURRENCY_COUNT))
 	total := 0
 	domenNames := 0
-	wg := sync.WaitGroup{}
 	gen := generators.SimpleGenerator{
-		Alphabet: *alphabet,
-		Len:      utils.Int(*urlLen),
+		Alphabet: *ALPHABET,
+		Len:      utils.Int(*URL_LEN),
 	}
-	writer, err := json.NewEachRowWriter("https.txt")
-	if err != nil {
-		fmt.Printf("Err:[%s]\n", err.Error())
-		os.Exit(-1)
-	}
+	protocolWriters := genWritersProtocols(PROTOCOLS)
 	for domenName := range gen.Gen() {
+		cuncurency.Wait()
 		total += 1
-		wg.Add(1)
-		go func(domen string, wg *sync.WaitGroup, w *json.EachRowWriter) {
-			defer wg.Done()
-			url := fmt.Sprintf("https://%s.ru", domen)
-			res, err := http.Get(url)
-			if err != nil {
-				fmt.Printf("Err:[%s]\n", err.Error())
-				return
-			}
-			defer res.Body.Close()
-			if res.StatusCode != 200 {
-				fmt.Printf("Status code [%d] error [%s]", res.StatusCode, res.Status)
-				return
-			}
-			doc, err := goquery.NewDocumentFromReader(res.Body)
-			if err != nil {
-				fmt.Printf("Err:[%s]\n", err.Error())
-				return
-			}
-			item := models.Page{}
-			doc.Find("title").Each(func(i int, s *goquery.Selection) {
-				item.Title = s.Text()
-			})
-			doc.Find("meta").Each(func(i int, s *goquery.Selection) {
-				if s.AttrOr("name", "") == "description" {
-					item.Description = s.AttrOr("content", "")
+		for _, protokol := range PROTOCOLS {
+			go func(protokol, domen string, w *json.EachRowWriter) {
+				defer cuncurency.Done()
+				url := fmt.Sprintf("%s://%s.ru", protokol, domen)
+				res, err := http.Get(url)
+				if err != nil {
+					fmt.Printf("Err:[%s]\n", err.Error())
+					return
 				}
-			})
-			err = w.WriteLine(item)
-			if err != nil {
-				fmt.Printf("Write line err:[%s]\n", err.Error())
-				return
-			}
-			domenNames += 1
-		}(domenName, &wg, writer)
+				defer res.Body.Close()
+				if res.StatusCode != 200 {
+					fmt.Printf("Status code [%d] error [%s]", res.StatusCode, res.Status)
+					return
+				}
+				doc, err := goquery.NewDocumentFromReader(res.Body)
+				if err != nil {
+					fmt.Printf("Err:[%s]\n", err.Error())
+					return
+				}
+				item := models.Page{}
+				doc.Find("title").Each(func(i int, s *goquery.Selection) {
+					item.Title = s.Text()
+				})
+				doc.Find("meta").Each(func(i int, s *goquery.Selection) {
+					if s.AttrOr("name", "") == "description" {
+						item.Description = s.AttrOr("content", "")
+					}
+				})
+				err = w.WriteLine(item)
+				if err != nil {
+					fmt.Printf("Write line err:[%s]\n", err.Error())
+					return
+				}
+				domenNames += 1
+			}(protokol, domenName, protocolWriters[protokol])
+		}
 	}
-	wg.Wait()
+	cuncurency.WaitAllDone()
 	println(fmt.Sprintf("Total size:[%d] Result:[%d]", total, domenNames))
 	/*	for key, value := range list {
 		fmt.Printf("Domen:[%s] Title:[%s] Description:[%s]\n", key, value.Title, value.Description)
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/digital-technology-agency/web-scan
 
 go 1.16
 
-require github.com/PuerkitoBio/goquery v1.7.1
+require (
+	github.com/PuerkitoBio/goquery v1.7.1
+	github.com/zenthangplus/goccm v0.0.0-20200608171100-39e9e08b694a
+)
